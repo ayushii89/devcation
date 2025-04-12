238557ae538934ddca8a99b9fb5ca1512a4239b1
Ayushee Yadav <144589455+ayushii89@users.noreply.github.com>
2025-04-12 22:29:02 +0530

    Add files via upload

---
+++ DEVCATION/script.js
@@ -0,0 +1,4 @@
+// script.js
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+<script src="https://kit.fontawesome.com/YOUR_KIT.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
